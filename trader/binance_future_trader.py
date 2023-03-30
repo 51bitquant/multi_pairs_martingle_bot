10987c8669b47acfb8e19ab4a85011eec433d1d0
xhl1993 <63508647+xhl1993@users.noreply.github.com>
2023-03-30 12:32:15 +0800

    Update binance_future_trader.py

--- trader/binance_future_trader.py
+++ trader/binance_future_trader.py
@@ -304,7 +304,7 @@ class BinanceFutureTrader(object):
 
                         price = bid_price * (1 + config.taker_price_pct)
                         price = round_to(price, min_price)
-                        qty = floor_to(buy_value / price, min_qty)
+                        qty = round_to(buy_value / price, min_qty)
 
                         buy_order = self.http_client.place_order(symbol=s, order_side=OrderSide.BUY,
                                                                  order_type=OrderType.LIMIT, quantity=qty,
